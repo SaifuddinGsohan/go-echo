45699ce13abdad58b55ac05522b8fa5fa1f02ec0
Saifuddin sohan <32895869+SaifuddinGsohan@users.noreply.github.com>
2023-06-19 13:34:18 +0600

    first commit

---
+++ api/handlers/posts_handler.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/SaifuddinGsohan/go-echo/api/service"
+	"github.com/labstack/echo"
+)
+
+func PostIndexHandler(c echo.Context) error {
+	data, err := service.GetAll()
+	if err != nil {
+		c.String(http.StatusBadGateway, "Unable to process data")
+	}
+	res := make(map[string]any)
+	res["status"] = "ok"
+	res["data"] = data
+	return c.JSON(http.StatusOK, res)
+}
+
+func PostSingleHandler(c echo.Context) error {
+	id := c.Param("id")
+	idx, err := strconv.Atoi(id)
+	if err != nil {
+		c.String(http.StatusBadGateway, "Unable to process data")
+	}
+	data, err := service.GetById(idx)
+	if err != nil {
+		c.String(http.StatusBadGateway, "Unable to process data")
+	}
+	res := make(map[string]any)
+	res["status"] = "ok"
+	res["data"] = data
+	return c.JSON(http.StatusOK, res)
+}
